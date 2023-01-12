292a150ce7ef043e60265409e8e3f7679948adc7
ximejuly <122545028+ximejuly@users.noreply.github.com>
2023-01-12 17:16:24 -0500

    Update RankingEcMedicos.sql

--- RankingEcMedicos.sql
+++ RankingEcMedicos.sql
@@ -1,10 +1,11 @@
 --PROCESO RANKING
-CREATE VIEW MedicosVal
+CREATE VIEW MedicosProducto
 AS (
                     SELECT   
                             		
-                            [b].[Cod_bi]
-                            ,[b].[Nom_bi]				             
+                             [b].[Cod_bi]
+                            ,[b].[Nom_bi]
+                            ,[m].[Producto]				             
                             ,SUM([m].[Val]) AS Suma
                     FROM [dbo].[Medicos] AS m
 
@@ -19,24 +20,55 @@ AS (
                         GROUP BY 
                                 		
                                 [b].[Cod_bi]
-                                ,[b].[Nom_bi]                   
+                            ,[b].[Nom_bi]
+                            ,[m].[Producto]                   
+                                    )
+
+--PROCESO RANKING
+CREATE VIEW MedicosMercado
+AS (
+                    SELECT   
+                            		
+                             [b].[Cod_bi]
+                            ,[b].[Nom_bi]
+                            ,[m].[Producto]
+                            ,[m].[Descripcion]				             
+                            ,SUM([m].[Val]) AS Suma
+                    FROM [dbo].[Medicos] AS m
+
+                        LEFT JOIN PAIS AS P
+                        ON P.Region=M.Region
+                        
+                        INNER JOIN  [dbo].[MedicosBi]AS b
+                        ON b.Cdg_medico=M.Cdg_medico
+                        
+                        WHERE M.MES=12 
+
+                        GROUP BY 
+                                		
+                                [b].[Cod_bi]
+                            ,[b].[Nom_bi]
+                            ,[m].[Producto]
+                            ,[m].[Descripcion]                   
                                     )
 
 
-
-
-
-
-
-
-
-
-SELECT [CodPais]
-      ,[Cod_bi]
+SELECT
+       [Cod_bi]
       ,[Nom_bi]
+      ,[Producto]
       ,[Suma]
-      ,RANK() OVER(PARTITION BY [CodPais] ORDER BY [Suma] DESC)
-FROM [BDMedicosLuminovaPg].[dbo].[MedicosVal]
+      ,DENSE_RANK() OVER(PARTITION BY [Cod_bi] ORDER BY [Suma] DESC) AS Rkn
+FROM [BDMedicosLuminovaPg].[dbo].[MedicosProducto]
+
+SELECT
+       [Cod_bi]
+      ,[Nom_bi]
+      ,[Descripcion]
+      ,[Suma]
+      ,DENSE_RANK() OVER(PARTITION BY [Nom_bi] ORDER BY [Suma] ASC) AS Rkn
+FROM [BDMedicosLuminovaPg].[dbo].[MedicosMercado]
+
 
 
 
