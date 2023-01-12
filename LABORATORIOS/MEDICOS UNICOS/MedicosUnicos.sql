d5bdb6f5b903d334927a9cd849673f8fd5595405
ximejuly <122545028+ximejuly@users.noreply.github.com>
2023-01-12 14:37:56 -0500

    Update MedicosUnicos.sql
    Update Katy

--- LABORATORIOS/MEDICOS UNICOS/MedicosUnicos.sql
+++ LABORATORIOS/MEDICOS UNICOS/MedicosUnicos.sql
@@ -1,18 +1,22 @@
 SELECT 
-         [m].[Cdg_medico]
-        ,[m].[Nombre]
-        ,[m].[Domicilio]
-        ,[m].[Localidad]
-        ,[m].[Region]
-        ,[m].[Esp]
-        ,[m].[Matricula]
+         RIGHT([p].[CodPais],2) AS PAIS
+		,[m].[Cdg_medico] AS CDG_MEDICO
+        ,[m].[Nombre] AS NOMBRE_DOCTOR
+        ,[m].[Domicilio] AS DOMICILIO
+        ,[m].[Localidad] AS LOCALIDAD
+        ,[m].[Region] AS REGION 
+        ,[m].[Esp] AS CDG_ESP1
+        ,[m].[Matricula] AS MATRICULA
 
-FROM [dbo].[Medicos] AS m
+FROM [BDMedicosLuminovaPg].[dbo].[Medicos] AS m 
+
+LEFT JOIN [BDMedicosLuminovaPg].[dbo].[Pais] AS [p] ON [p].[Region]=m.[Region]
 
 WHERE [Mes]=12
 
 GROUP BY
-          [m].[Cdg_medico]
+         [p].[CodPais]
+        ,[m].[Cdg_medico]
         ,[m].[Nombre]
         ,[m].[Domicilio]
         ,[m].[Localidad]
